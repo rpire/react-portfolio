106bb5870470fd22251d4c0a2fd888302d2008d3
rpire <rubenpire7@gmail.com>
2022-02-03 00:10:31 -0400

    Partially implement the Portfolio mobile version

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navigation from './components/Navigation';
 import Hello from './components/Hello';
+import Portfolio from './components/Portfolio';
 
 const App = () => (
   <>
     <Navigation />
     <Hello />
+    <Portfolio />
   </>
 );
 
---
+++ src/components/Project.js
@@ -0,0 +1,40 @@
+import PropTypes from 'prop-types';
+import { IoArrowForwardSharp } from 'react-icons/io5';
+import Badge from './Badge';
+
+const Project = (props) => {
+  const { project } = props;
+
+  return (
+    <li>
+      <article className="project-card">
+        <img
+          src={project.image}
+          alt="project_image"
+          className="project-img"
+        />
+        <div className="project-info">
+          <h3 className="project-title">{project.title}</h3>
+          <ul className="tech-list">
+            {project.technologies.map((technology) => (
+              <Badge key={technology.id} tech={technology.tech} />
+            ))}
+          </ul>
+          <button
+            type="button"
+            className="project-btn"
+          >
+            See this project
+            <IoArrowForwardSharp className="arrow-right" />
+          </button>
+        </div>
+      </article>
+    </li>
+  );
+};
+
+Project.propTypes = {
+  project: PropTypes.instanceOf(Object).isRequired,
+};
+
+export default Project;
