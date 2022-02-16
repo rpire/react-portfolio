3212f59099ed17a547ae9dc71514ace2bf516f10
rpire <rubenpire7@gmail.com>
2022-02-15 21:13:26 -0400

    Import the Contact component in App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navigation from './components/Navigation';
 import Hello from './components/Hello';
 import Portfolio from './components/Portfolio';
 import About from './components/About';
+import Contact from './components/Contact';
 
 const App = () => (
   <>
@@ -9,6 +10,7 @@ const App = () => (
     <Hello />
     <Portfolio />
     <About />
+    <Contact />
   </>
 );
 
