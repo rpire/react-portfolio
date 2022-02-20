dad69f8a1208f1907b123e72878987cd014b1b2b
rpire <rubenpire7@gmail.com>
2022-02-20 17:01:05 -0400

    Implement changes for medium screens

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,4 +1,5 @@
 import Project from './Project';
+import PortfolioHeader from './PortfolioHeader';
 
 const Portfolio = () => {
   const projects = [
@@ -74,15 +75,67 @@ const Portfolio = () => {
       ],
       url: ['url.one', 'url.two'],
     },
+    {
+      id: 5,
+      title: 'Mi mama me mima',
+      description: 'Mi mama me ama',
+      image: './media/work-img-5.svg',
+      technologies: [
+        {
+          id: 1,
+          tech: 'React/Redux',
+        },
+        {
+          id: 2,
+          tech: 'Ruby on Rails',
+        },
+      ],
+      url: ['url.one', 'url.two'],
+    },
+    {
+      id: 6,
+      title: 'Mi mama me mima',
+      description: 'Mi mama me ama',
+      image: './media/work-img-5.svg',
+      technologies: [
+        {
+          id: 1,
+          tech: 'React/Redux',
+        },
+        {
+          id: 2,
+          tech: 'Ruby on Rails',
+        },
+      ],
+      url: ['url.one', 'url.two'],
+    },
   ];
 
   return (
     <section className="portfolio">
-      <h2 id="portfolio">Projects</h2>
-      <ul>
-        {projects.map((project) => (
-          <Project key={project.id} project={project} />
-        ))}
+      <ul className="work-list">
+        <PortfolioHeader />
+        <li className="project-1">
+          <Project project={projects[0]} />
+        </li>
+        <li className="project-2">
+          <Project project={projects[1]} />
+        </li>
+        <li className="project-3">
+          <Project project={projects[0]} />
+        </li>
+        <li className="project-4">
+          <Project project={projects[1]} />
+        </li>
+        <li className="project-5">
+          <Project project={projects[0]} />
+        </li>
+        <li className="project-6">
+          <Project project={projects[1]} />
+        </li>
+        <div className="work-deco">
+          <img src="./media/image_dots.svg" alt="" className="work-dots work-img" />
+        </div>
       </ul>
     </section>
   );
