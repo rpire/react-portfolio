31b49b8c6a7d63e8e8f5f802f9debe7a4da4d830
rpire <rubenpire7@gmail.com>
2022-02-21 20:54:39 -0400

    Change className .centered-btn to .body-btn

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -6,10 +6,10 @@ const Hello = () => (
       or website. Take a look at my works. If you like what you see and have a
       project you need coded, don&apos;t hesitate and contact me.
     </p>
-    <a href="#contact" className="centered-btn" rel="noopener noreferrer">
+    <a href="#contact" className="body-btn" rel="noopener noreferrer">
       Start Collaboration
     </a>
-    <img src="./media/image_geometry_2.svg" alt="" id="half-mosaic" />
+    <img src="./media/image_geometry_2.svg" alt="" className="half-mosaic" />
   </section>
 );
 
